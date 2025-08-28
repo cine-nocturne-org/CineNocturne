3d5a1b7dbf5e95dc8d0fcef3b50df26c1d230428
Lou <lou.duperret@gmail.com>
2025-08-28 16:13:32 +0200

    Update config.py

--- E3_E4_API_app/config.py
+++ E3_E4_API_app/config.py
@@ -1,23 +1,24 @@
 # config.py
 import os
 import mlflow
+import pathlib
 
 # ------------------------------
 # PostgreSQL Render pour MLflow
 # ------------------------------
 DB_URL = "postgresql+psycopg2://mlflow_db_evzp_user:ZIG9DNZ2gWeqCOQ7SkjIM7cZI9tJcp53@dpg-d2o5rour433s73avu490-a.frankfurt-postgres.render.com/mlflow_db_evzp"
 
-# Dossier local pour les artefacts (tu peux changer)
-import pathlib
-MLFLOW_ARTIFACT_LOCATION = str(pathlib.Path(__file__).parent / "mlflow_artifacts")
+# Variable pour compatibilité API
+MLFLOW_TRACKING_URI = DB_URL
 
-# Crée le dossier artefacts s'il n'existe pas
+# Dossier local pour les artefacts (tu peux changer)
+MLFLOW_ARTIFACT_LOCATION = str(pathlib.Path(__file__).parent / "mlflow_artifacts")
 pathlib.Path(MLFLOW_ARTIFACT_LOCATION).mkdir(exist_ok=True)
 
 # ------------------------------
 # Configuration MLflow
 # ------------------------------
-mlflow.set_tracking_uri(DB_URL)
+mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
 
 # Créer l'expérience si elle n'existe pas
 EXPERIMENT_NAME = "louve_movies_monitoring"
