0f19b07830a3e6b5d9e26b8c01607a534caa5705
Lou <lou.duperret@gmail.com>
2025-09-11 15:44:59 +0200

    Update api_movie_v2.py

--- E3_E4_API_app/api_movie_v2.py
+++ E3_E4_API_app/api_movie_v2.py
@@ -75,6 +75,7 @@ DATABASE_URL = os.getenv(
     "MYSQL_URL",
     "mysql+pymysql://louve:%40Marley080922@mysql-louve.alwaysdata.net/louve_movies"
 )
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
 def get_engine():
     """Singleton SQLAlchemy engine (lazy)."""
@@ -1143,3 +1144,4 @@ async def _debug_catalog():
         }
     except Exception as e:
         return {"error": str(e)}
+
